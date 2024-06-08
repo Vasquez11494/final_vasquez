4365df0441f0268ae9eac4acc6c21fb57dc159b6
Vasquez11494 <cvasquez857@gmail.com>
2024-06-08 14:57:31 -0600

    se creo la funcion para ver las materias ingresadas

--- controladores/materias/buscar.php
+++ controladores/materias/buscar.php
@@ -4,3 +4,85 @@
 // error_reporting(E_ALL);
 
 require '../../modelos/Materia.php';
+
+
+try {
+
+    $VerMaterias = new Materia();
+    $Materias = $VerMaterias->VerMaterias();
+    $resultado = [
+        'mensaje' => 'EXITO BUSCANDO MATERIAS',
+        'codigo' => 1
+    ];
+
+} catch (PDOException $pe) {
+    $resultado = [
+        'mensaje' => 'ERROR BUSCANDO LAS MATERIAS EN LA BASE DE DATOS',
+        'detalle' => $pe->getMessage(),
+        'codigo' => 0
+    ];
+} catch (Exception $e) {
+    $resultado = [
+        'mensaje' => 'OCURRIO UN ERROR EN LA EJECUCIÓN',
+        'detalle' => $e->getMessage(),
+        'codigo' => 0
+    ];
+}
+$alertas = ['danger', 'success', 'warning'];
+
+
+include_once '../../vistas/templates/header.php'; ?>
+
+<div class="row justify-content-center mt-3">
+    <div class="col-lg-6 alert alert-<?=$alertas[$resultado['codigo']] ?>" role="alert">
+        <?= $resultado['mensaje'] ?>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-lg-6">
+        <a href="../../vistas/materias/index.php" class="btn btn-primary w-100">Regresar</a>
+    </div>
+</div>
+
+<h1 class="text-center ">MATERIAS INGRESADAS </h1>
+    <div class="row justify-content-center">
+        <div class="col-lg-10">
+            <table class="table table-bordered table-hover">
+                <thead> 
+                    <tr>
+                        <th>No.</th>
+                        <th>Nombre</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if($resultado['codigo'] == 1 ) : ?>
+                        <?php foreach ($Materias as $key => $materia) : ?>
+                            <tr>
+                                <td><?= $key + 1 ?></td>
+                                <td><?= $materia['materia_nombre'] ?></td>
+                                <td class="text-center">
+                                <div class="dropdown">
+                                    <button class="btn btn-info dropdown-toggle"  style=" background-color: rgba(23, 192, 204, 0.641)" ="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Acciones
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="../../vistas/alumnos/modificar.php?alu_id=<?= base64_encode($value['alu_id'])?>"><i class="bi bi-pencil-square me-2"></i>Modificar</a></li>
+                                        <li><a class="dropdown-item" href="eliminar.php?alu_id=<?= base64_encode( $value['alu_id'])?>"><i class="bi bi-trash me-2"></i>Eliminar</a></li>
+                                    </ul>
+                                </div>
+                                </td>
+                            </tr>
+                        <?php endforeach ?>
+                    <?php else : ?>
+                        <tr>
+                            <td colspan="3">No hay productos registrados</td>
+                        </tr>  
+                    <?php endif ?>
+                </tbody>
+                        
+            </table>
+        </div>        
+    </div>        
+
+<?php include_once '../../vistas/templates/footer.php'; ?>  
--- modelos/Materia.php
+++ modelos/Materia.php
@@ -23,4 +23,11 @@ class Materia extends Conexion{
         return $resultado;
     }
 
+    public function VerMaterias (){
+        $sql = "SELECT * FROM materias";
+
+        $resultado =  self::servir($sql);
+        return $resultado;
+    }
+
 }
